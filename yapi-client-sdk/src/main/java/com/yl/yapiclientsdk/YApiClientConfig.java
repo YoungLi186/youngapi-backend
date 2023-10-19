91e1597abb091c8e03e7466c9be0415a3e8287ce
LiYang186 <1443510186@qq.com>
2023-10-19 15:46:18 +0800

    pref: 调用接口SDK

---
+++ yapi-client-sdk/src/main/java/com/yl/yapiclientsdk/YApiClientConfig.java
@@ -0,0 +1,30 @@
+package com.yl.yapiclientsdk;
+
+import com.yl.yapiclientsdk.client.YApiClient;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Date: 2023/10/14 - 10 - 14 - 14:32
+ * @Description: com.yl.yapiclientsdk
+ */
+@Configuration
+@ConfigurationProperties("yapi.client")
+@Data
+@ComponentScan
+public class YApiClientConfig {
+
+    private String accessKey;
+
+    private String secretKey;
+
+    @Bean
+    public YApiClient yApiClient() {
+        return new YApiClient(accessKey, secretKey);
+    }
+
+
+}
